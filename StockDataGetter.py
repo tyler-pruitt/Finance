97c071047a846622220c216486c5669b7b0d82f9
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2022-04-10 02:10:59 -0700

    Add files via upload

---
+++ StockDataGetter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 10 01:52:09 2022
+
+@author: tylerpruitt
+"""
+
+import yfinance as yf
+import pandas as pd
+
+tickerStrings = ['WARFY', 'GOOGL']
+df_list = list()
+for ticker in tickerStrings:
+    data = yf.download(ticker, group_by="Ticker", period='2d', interval='1m')
+    data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
+    df_list.append(data)
+
+# combine all dataframes into a single dataframe
+df = pd.concat(df_list)
+
+# save to csv
+df.to_csv('ticker.csv')
